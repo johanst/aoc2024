d67f4ad97b5f03304e80a6d1e64b3937999029e0
Johan Sternerup <johan.sternerup@gmail.com>
2024-12-01 06:31:59 +0100

    Day1 - get to know splitWhitespace() in nim
    And polish the solution.

--- day01/prog.nim
+++ day01/prog.nim
@@ -1,14 +1,13 @@
 import strutils
 import algorithm
-import tables
 
 proc getInput(fname: string): (seq[int], seq[int]) =
   var a: seq[int] = @[]
   var b: seq[int] = @[]
-  for line in lines("input.txt"):
-    let w = line.strip().split()
+  for line in lines(fname):
+    let w = line.strip().splitWhitespace()
     a.add(parseInt(w[0]))
-    b.add(parseInt(w[^1]))
+    b.add(parseInt(w[1]))
   return (a, b)
 
 proc part1(fname: string): int =
@@ -21,15 +20,10 @@ proc part1(fname: string): int =
 
 proc part2(fname: string): int =
   var (a, b) = getInput(fname)
-  var sim: Table[int, int]
+  result = 0
   for n in b:
     if a.contains(n):
-      var count = sim.getOrDefault(n, 0)
-      count += 1
-      sim[n] = count
-  result = 0
-  for k, v in sim:
-    result += k * v
+      result += n
 
 echo "Part1: ", part1("input.txt")
 echo "Part2: ", part2("input.txt")
