53589f94a2341a961910e4f0398fcd6299190b4b
Johan Sternerup <johan.sternerup@gmail.com>
2024-12-06 07:22:13 +0100

    Day 6 - done very inefficiently
    Algorithm can be improved and probably i pass things by value.

--- day06/prog.nim
+++ day06/prog.nim
@@ -1,7 +1,6 @@
 import strutils
 import sequtils
-import algorithm
-import tables
+import sets
 
 type
   Data = object
@@ -62,11 +61,69 @@ proc getNumVisited(fname: string): int =
 
 assert getNumVisited("ex0.txt") == 41
 
+type GuardState =
+  tuple[x: int, y: int, dx: int, dy: int]
+
+proc hasLoop(d: Data, xo, yo: int): bool =
+  var v = initHashSet[GuardState]()
+  v.incl((x: d.x0, y: d.y0, dx: 0, dy: -1))
+
+  let ysz = d.map.len
+  let xsz = d.map[0].len
+  var x = d.x0
+  var y = d.y0
+  var dx = 0
+  var dy = -1
+  while true:
+    var xx = x + dx
+    var yy = y + dy
+    # echo (x: x, y: y, dx: dx, dy: dy)
+    if xx < 0 or xx >= xsz or yy < 0 or yy >= ysz:
+      # echo "false after ", v.len
+      return false
+    var c = d.map[yy][xx]
+    if yy == yo and xx == xo:
+      c = '#'
+    if c == '#':
+      if (dy, dx) == (-1, 0):
+        (dy, dx) = (0, 1)
+      elif (dy, dx) == (0, 1):
+        (dy, dx) = (1, 0)
+      elif (dy, dx) == (1, 0):
+        (dy, dx) = (0, -1)
+      elif (dy, dx) == (0, -1):
+        (dy, dx) = (-1, 0)
+      else:
+        assert false, "WTF"
+    else:
+      y = yy
+      x = xx
+    let st = (x: x, y: y, dx: dx, dy: dy)
+    if v.contains(st):
+      # echo "true after ", v.len
+      return true
+    v.incl(st)
+
+proc getNumLoops(fname: string): int =
+  let d = getInput(fname)
+  for y in 0..<d.map.len:
+    for x in 0..<d.map[0].len:
+      if y == d.y0 and x == d.x0 or d.map[y][x] == '#':
+        continue
+      if hasLoop(d, x, y):
+        result += 1
+
+let exData = getInput("ex0.txt")
+assert hasLoop(exData, exData.x0 - 1, exData.y0)
+
+echo getNumLoops("ex0.txt")
+assert getNumLoops("ex0.txt") == 6
+
 proc part1(fname: string): int =
   return getNumVisited("input.txt")
 
 proc part2(fname: string): int =
-  return 0
+  return getNumLoops("input.txt")
 
 echo "Part1: ", part1("input.txt")
 echo "Part2: ", part2("input.txt")
