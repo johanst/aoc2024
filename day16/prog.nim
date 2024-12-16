088c48d9335c2e3f89168c116738edfa0a93f8ff
Johan Sternerup <johast@axis.com>
2024-12-16 07:58:14 +0100

    Day 16
    Change-Id: I11352487b0bbba41d325699518c424742501e0c3

--- day16/prog.nim
+++ day16/prog.nim
@@ -19,6 +19,7 @@ type
     cost: int
     x, y: int
     dx, dy: int
+    path: seq[Pos]
 
 proc `<`(a, b: State): bool =
   a.cost < b.cost
@@ -46,6 +47,7 @@ proc getInitState(d: Data): State =
   s.y = d.s.y
   s.dx = 1
   s.dy = 0
+  s.path.add((x: s.x, y: s.x))
   return s
 
 proc drawMap(d: Data, s: State) =
@@ -104,19 +106,57 @@ proc getMinCost(fname: string): int =
       v[vv] = cost
       let sn = State(cost: cost, x: p.x, y: p.y, dx: dirn[0], dy: dirn[1])
       hq.push(sn)
-    # echo hq
-    # echo v
-    # drawMap(d, s)
-    # assert false, "remove this"
   return 0
 
 assert getMinCost("ex0.txt") == 7036
+
+proc getNumSeats(fname: string): int =
+  let shortest_path = getMinCost(fname)
+  let d = getInput(fname)
+  var seats: HashSet[Pos]
+  let s0 = getInitState(d)
+  var v: Table[(Pos, Pos), int] # posx, posy, dx, dy -> cost
+  var hq: HeapQueue[State]
+  hq.push(s0)
+  v[((s0.x, s0.y), (s0.dx, s0.dy))] = 0
+  while hq.len > 0:
+    let s = hq.pop()
+    if (s.x, s.y) == (d.e.x, d.e.y):
+      for seat in s.path:
+        seats.incl(seat)
+      continue # there may be more
+    let dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+    var dirIdx = 0
+    while (s.dx, s.dy) != dir[dirIdx]:
+      dirIdx += 1
+    for i in -1..1:
+      var cost = 1001 # turn + forward
+      if i == 0:
+        cost = 1 # just forward
+      cost += s.cost
+      if cost > shortest_path:
+        continue
+      let dirn = dir[umod(dirIdx + i, 4)]
+      let p = (x: s.x + dirn[0], y: s.y + dirn[1])
+      if d.m[p.y][p.x] == '#':
+        continue
+      let vv = (p, (dirn[0], dirn[1]))
+      if v.contains(vv):
+        if cost > v[vv]:
+          continue
+      v[vv] = cost
+      var sn = State(cost: cost, x: p.x, y: p.y, dx: dirn[0], dy: dirn[1], path: s.path)
+      sn.path.add((x: p.x, y: p.y))
+      hq.push(sn)
+  return seats.len
+
+assert getNumSeats("ex0.txt") == 45
 
 proc part1(fname: string): int =
   return getMinCost("input.txt")
 
 proc part2(fname: string): int =
-  return 0
+  return getNumSeats("input.txt")
 
 echo "Part1: ", part1("input.txt")
 echo "Part2: ", part2("input.txt")
